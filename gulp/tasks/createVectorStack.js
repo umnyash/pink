88562eb2fa02b1b6d19080c2acf3dbd754ad72fb
Rustam <rustam17.sb@gmail.com>
2023-05-24 23:38:50 +0300

    init: add site files

---
+++ gulp/tasks/createVectorStack.js
@@ -0,0 +1,19 @@
+import gulp from 'gulp';
+
+// Configuration
+import mode from '../mode.js';
+import paths from '../paths.js';
+
+// Plugins
+import browser from 'browser-sync';
+import gulpIf from 'gulp-if';
+import { stacksvg } from 'gulp-stacksvg';
+import svgmin from 'gulp-svgmin';
+
+export default function createVectorStack() {
+  return gulp.src(paths.createVectorStack.src)
+    .pipe(stacksvg())
+    .pipe(gulpIf(mode.isProd, svgmin()))
+    .pipe(gulp.dest(paths.createVectorStack.dest))
+    .pipe(gulpIf(mode.isDev, browser.stream()));
+}
---
+++ gulp/tasks/processScripts.js
@@ -0,0 +1,17 @@
+import gulp from 'gulp';
+
+// Configuration
+import mode from '../mode.js';
+import paths from '../paths.js';
+
+// Plugins
+import browser from 'browser-sync';
+import gulpIf from 'gulp-if';
+import webpack from 'webpack-stream';
+
+export default function processScripts() {
+  return gulp.src(paths.processScripts.src, { sourcemaps: mode.isDev })
+    .pipe(webpack({ mode: mode.isProd ? 'production' : 'development' }))
+    .pipe(gulp.dest(paths.processScripts.dest, { sourcemaps: mode.isDev }))
+    .pipe(gulpIf(mode.isDev, browser.stream()));
+}
