08c9291c3976cbbc9eb7abf1446800a8447dc4c0
Rustam <rustam17.sb@gmail.com>
2023-07-02 15:51:32 +0300

    fix: make the hidden navigation menu inaccessible from the keyboard

--- source/js/main-navigation.js
+++ source/js/main-navigation.js
@@ -1,15 +1,27 @@
+const ANIMATION_DURATION = 300;
+
 const initMainNavigation = (navigationElement) => {
-  navigationElement.classList.remove('main-header__navigation--no-js');
+  navigationElement.classList.add('main-header__navigation--js');
 
   const toggleButtonElement = navigationElement.querySelector('.main-header__burger');
+  const menuElement = navigationElement.querySelector('.main-header__main-menu');
 
   toggleButtonElement.addEventListener('click', () => {
     toggleButtonElement.classList.remove('burger--animated');
 
-    toggleButtonElement.classList.toggle('burger--close');
+    setTimeout(() => {
+      toggleButtonElement.classList.toggle('burger--close');
+      toggleButtonElement.classList.add('burger--animated');
+    }, 1);
+
+    if (navigationElement.classList.contains('main-header__navigation--open')) {
+      setTimeout(() => menuElement.classList.remove('main-header__main-menu--animated'), ANIMATION_DURATION);
+    } else {
+      menuElement.classList.add('main-header__main-menu--animated');
+    }
+
     navigationElement.classList.toggle('main-header__navigation--open');
 
-    setTimeout(() => toggleButtonElement.classList.add('burger--animated'), 0);
   });
 };
 
